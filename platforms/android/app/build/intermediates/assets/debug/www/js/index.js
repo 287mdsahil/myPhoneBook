10f314a356cebcd81072f044346eaebe3250c856
Md Sahil <287mdsahil@gmail.com>
2018-09-12 00:26:02 +0530

    added navigator.contacts.find
    displayed no of contacts in a alert box

--- platforms/android/app/build/intermediates/assets/debug/www/js/index.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -1,12 +1,18 @@
-function dummy()
+/*function dummy()
 {
 	window.alert("test");
 	console.log("test");
 }
+*/
 
+function onSuccess(c)
+{
+	window.alert("contacts found: " + c.length);
+}
 
 document.addEventListener("deviceready", onDeviceReady);
 function onDeviceReady() 
 {
-	window.alert("ready");
+	var fields = [navigator.contacts.fieldType.name];
+	navigator.contacts.find(fields, onSuccess);
 }
