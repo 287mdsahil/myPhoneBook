c351a750d6120f9e69f83b92f01151d660e98458
Md Sahil <287mdsahil@gmail.com>
2018-09-15 21:41:18 +0530

    added contact images to display
    sorting contacts still not working

--- platforms/android/app/build/intermediates/assets/debug/www/js/index.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -1,12 +1,32 @@
-/*function dummy()
-{
-	window.alert("test");
-	console.log("test");
-}
-*/
 
+//sorts the contacts in alphabetical order of the displayName-------------------------------
+function contactSort(c)
+{
+	for(var i=0;i<c.length-1;i++)
+	{
+		var m=i;
+		for(var j=i+1;j<c.length;j++)
+		{
+			if(c[j].displayName>c[m].displayName)
+				m=j;
+				console.log(c[j].displayName + "<" + c[m].displayName);
+		}
+		
+		var dummy = navigator.contacts.create();
+		dummy=c[m];
+		c[m]=c[i];
+		c[i]=c[m];
+	}
+	return c;
+}
+
+
+//stores the contacts and displays them the table view--------------------------------------
 function onSuccess(c)
 {
+	console.dir(c);
+	//c=contactSort(c);
+	console.dir(c);
 	//window.alert("contacts found: " + c.length);
 	var table = document.getElementById("contact_list");
 	var i=0;
@@ -18,7 +38,13 @@ function onSuccess(c)
 		var name = c[i].displayName;
 		var cell1 = row.insertCell(0);
 		var img = document.createElement('img');
-		img.src="img/default-user-img.png";
+		
+		if(c[i].photos!=null)
+			img.src=c[i].photos[0].value;
+		else
+			img.src="img/default-user-img.png";
+		
+		
 		cell1.appendChild(img);
 		var nameDiv = document.createElement('div');
 		nameDiv.innerHTML = name;
@@ -44,6 +70,9 @@ function onSuccess(c)
 document.addEventListener("deviceready", onDeviceReady);
 function onDeviceReady() 
 {
-	var fields = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.phoneNumbers];
+	var fields = 	[navigator.contacts.fieldType.displayName, 
+					navigator.contacts.fieldType.phoneNumbers, 
+					navigator.contacts.fieldType.photos];
+					
 	navigator.contacts.find(fields, onSuccess);
 }
